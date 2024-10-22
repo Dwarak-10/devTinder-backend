454c0be27828fd5dfeed0e040848b7f99305d2de
Dwarak-10 <dwarakeshvaran0512@gmail.com>
2024-10-22 17:15:17 +0530

    explore GET,POST,DELETE

--- src/app.js
+++ src/app.js
@@ -2,16 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.get("/feed", (req, res) => {
-  res.send("Welcome to the nodejs project");
+//Order of the routes matters alot
+
+//This will only call the GET call to the /user
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Dwarakesh",
+    lastName: "DK",
+    mobileNo: 9080214419,
+  });
 });
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the dashboard");
+//This will only call the POST call to the user
+app.post("/user", (req, res) => {
+  res.send("Send Data successfully");
 });
-app.get("/test", (req, res) => {
+
+//This will only call the POST call to the user
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully");
+});
+
+// This will match all the HTTP methos API calls to /test
+app.use("/test", (req, res) => {
   res.send("Hello test page");
 });
+
 app.listen(3000, () => {
   console.log("Server starting at port 3000");
 });
