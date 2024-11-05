925902d96d564f6acc62b7161b1ecbc9c25eeb5d
Dwarak-10 <dwarakeshvaran0512@gmail.com>
2024-11-05 19:01:31 +0530

    API connection request accept/reject

--- src/router/request.js
+++ src/router/request.js
@@ -22,7 +22,6 @@ requestRouter.post(
       }
 
       const toUser = await User.findById(toUserId);
-      console.log(toUser.firstName);
 
       if (!toUser) {
         return res.status(404).json({ message: "User not found!" });
@@ -57,6 +56,42 @@ requestRouter.post(
       });
     } catch (err) {
       res.status(400).send("Somethinf went wrong" + err);
+    }
+  }
+);
+
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+
+      const allowedStatus = ["accepted", "rejected"];
+
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: "Status not allowed" });
+      }
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        status: "interested",
+        toUserId: loggedInUser._id,
+      });
+
+      if (!connectionRequest) {
+        return res
+          .status(400)
+          .json({ message: "Connection request not found" });
+      }
+
+      connectionRequest.status = status;
+
+      const data = await connectionRequest.save();
+
+      res.json({ message: `Connection request ${status}`, data });
+    } catch (err) {
+      res.status(400).send(`Error : ${err.message}`);
     }
   }
 );
