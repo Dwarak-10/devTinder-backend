0cad6410b66611d665d2c687fc62adfcc564a142
Dwarak-10 <dwarakeshvaran0512@gmail.com>
2024-12-03 12:35:58 +0530

    final

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
+    // console.log("Monog_URI : ", MONGO_URI);
     await mongoose.connect(MONGO_URI);
   } catch (err) {
     console.log(err);
